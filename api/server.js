6912aa3f5c4382bf71e77a4ff115f801b4dca949
waiyanmyosat <138376625+waiyanmyosat@users.noreply.github.com>
2023-07-08 00:24:04 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,18 +1,19 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
-
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
 server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router)
-
-const PORT = 8000
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
 })
+
+// Export the Server API
+module.exports = server
